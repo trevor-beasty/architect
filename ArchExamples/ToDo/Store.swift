d0df263692889e6e379648f6705e271502b38ae6
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-01-16 17:20:53 -0500

    generic store

--- ArchExamples/ToDo/Store.swift
+++ ArchExamples/ToDo/Store.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import RxSwift
+
+class Store<State, Intent, Change> where State: Equatable, Intent: Equatable {
+    
+    let stateSubject: Variable<State>
+    let intentSubject = PublishSubject<Intent>()
+    private let bag = DisposeBag()
+    
+    let reduceIntent: IntentReducer
+    let reduceChange: ChangeReducer
+    
+    typealias IntentReducer = (Intent, () -> State) -> Observable<Change>
+    typealias ChangeReducer = (Change, () -> State) -> State
+    
+    init(state: State, reduceIntent: @escaping IntentReducer, reduceChange: @escaping ChangeReducer) {
+        self.stateSubject = Variable(state)
+        self.reduceIntent = reduceIntent
+        self.reduceChange = reduceChange
+    }
+    
+    func subscribe() {
+        
+        intentSubject.asObservable()
+            .flatMap({
+                return self.reduceIntent($0, { return self.state })
+            })
+            .subscribe(onNext: {
+                let newState = self.reduceChange($0, { return self.state })
+                self.update(newState)
+            })
+            .disposed(by: bag)
+        
+    }
+    
+    private var state: State {
+        return stateSubject.value
+    }
+    
+    private func update(_ state: State) {
+        stateSubject.value = state
+    }
+    
+}
